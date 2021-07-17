a9ebacf926ef9f754fa42bf25ab8551f399eeb19
zahidalik <zahidkhan545@icloud.com>
2021-07-17 09:00:42 +0300

    add beem.africa api

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,16 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(:product_params)
+
+    if @product.save
+      Product.sms_alert
+      flash[:alert] = "Please check the sms send to your mobile number"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,31 @@ class Product < ApplicationRecord
   
   has_rich_text :description
   has_many_attached :images
+
+  def self.sms_alert
+    require 'net/http'
+    require 'json'
+
+    api_key = Rails.application.credentials.beem[:api_key]
+    secret_key = Rails.application.credentials.beem[:secret_key]
+    source_addr = localhost:3000
+    body = {
+      'source_addr': source_addr,
+      'schedule_time': '',
+      'encoding': '0',
+      'message': 'SMS Test from Ruby API.',
+      'recipients': [
+      {
+      'recipient_id': 1,
+      'dest_addr': '255655719784', # this will change with the mobile number of the user
+      },
+      ],
+      }
+    uri = URI('https://apisms.beem.africa/v1/send')
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Authorization' => 'Basic ' + api_key + ':' + secret_key)
+    req.body = {body}.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+  end
 end
